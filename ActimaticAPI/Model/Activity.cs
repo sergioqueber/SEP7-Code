894e007b3c0905430fb985bf99513330f57d730c
natalkawajda <316345@via.dk>
2024-11-11 17:15:09 +0100

    activity classes implementation

---
+++ ActimaticAPI/Model/Activity.cs
@@ -0,0 +1,17 @@
+namespace Model;
+
+public abstract class Activity 
+{
+    public int Id {get; set;}
+    public string Name {get; set;}
+    public int AwardedPoints {get; set}
+    public DateTime Date {get; set;}
+}
+
+public Activity(int id, string name, int awardedPoints, DateTime date)
+{
+    Id = id;
+    Name = name;
+    AwardedPoints = awardedPoints;
+    Date = date;
+}
