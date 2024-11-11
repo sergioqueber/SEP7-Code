e8f6392b3a3ba2b4ee01039c3be0c63d10dc6e6b
marcintyminski <mtyminski8@gmail.com>
2024-11-11 17:15:43 +0100

    Report Model

---
+++ ActimaticAPI/Model/Report.cs
@@ -0,0 +1,23 @@
+using System.Diagnostics;
+
+namespace Model;
+
+public class Report{
+    public int Id {get; set;}
+    public DateTime StartDate {get; set;}
+    public DateTime EndDate {get; set;}
+    public List<Staff>? ActiveParticipants {get; set;}
+    public List<Reward>? AwardedRewards {get; set;}
+    public int EmissionsSaved {get; set;}
+    public List<Activity>? CompletedActivities {get; set;}
+
+public Report(int id, DateTime startDate, DateTime endDate, List<Staff> activeParticipants, List<Reward> awaredeRewards, int emissionsSaved, List<Activity>? completedActivities){
+    Id = id;
+    StartDate = startDate;
+    EndDate = endDate;
+    ActiveParticipants = activeParticipants;
+    AwardedRewards = awaredeRewards;
+    EmissionsSaved = emissionsSaved;
+    CompletedActivities = completedActivities;
+}
+}
