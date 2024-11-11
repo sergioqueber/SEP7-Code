63c07db792cb50b5016ac58eecae40082a69c73f
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2024-11-11 18:02:12 +0100

    Implement CRUD operations in StaffService and update IStaffService interface

--- ActimaticAPI/Interfaces/IStaffService.cs
+++ ActimaticAPI/Interfaces/IStaffService.cs
@@ -3,6 +3,10 @@ using Model;
 public interface IStaffService
 {
     Task<IEnumerable<Staff>> GetAllStaff();
-     Task<Staff> GetStaffById(int id);
+    Task<Staff> GetStaffById(int id);
+    Task<Staff> CreateStaff(Staff staff);
+    Task<Staff> UpdateStaff(Staff staff);
+    Task<Staff> RemoveStaff(int id);    
+
     
 }
