4976a8d1a8fd5517708abd798285a4d18c6c9a32
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2024-11-11 18:04:39 +0100

    Add StaffController with CRUD operations for staff management

---
+++ ActimaticAPI/Controller/StaffController.cs
@@ -0,0 +1,45 @@
+namespace Controller;
+using Model;
+using Services;
+using Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+public class StaffController : ControllerBase
+{
+    private readonly IStaffService _staffService;
+
+    public StaffController(IStaffService staffService)
+    {
+        _staffService = staffService;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<Staff>> GetAllStaff()
+    {
+        return await _staffService.GetAllStaff();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<Staff> GetStaffById(int id)
+    {
+        return await _staffService.GetStaffById(id);
+    }
+
+    [HttpPost]
+    public async Task<Staff> CreateStaff([FromBody] Staff staff)
+    {
+        return await _staffService.CreateStaff(staff);
+    }
+
+    [HttpPut]
+    public async Task<Staff> UpdateStaff([FromBody] Staff staff)
+    {
+        return await _staffService.UpdateStaff(staff);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<Staff> RemoveStaff(int id)
+    {
+        return await _staffService.RemoveStaff(id);
+    }
+}
