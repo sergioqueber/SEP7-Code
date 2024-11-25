85e45c7a2ec64707045811ff6b4ccb4f5dc5aefd
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2024-11-25 17:41:32 +0100

    Implement Admin management functionality with CRUD operations and update Staff model

--- ActimaticAPI/Program.cs
+++ ActimaticAPI/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddSingleton<ISavingFoodService, SavingFoodService>();
 builder.Services.AddSingleton<IDepartmentService, DepartmentService>();
 builder.Services.AddSingleton<IRewardService, RewardService>();
 builder.Services.AddSingleton<ITeamService, TeamService>();
-
+builder.Services.AddSingleton<IAdminService, AdminService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
