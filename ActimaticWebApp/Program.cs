ad1f64270f7353224419d5c1deef8fdf1bf72041
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2024-12-10 11:29:54 +0100

    Implement dynamic ID generation for entities, enhance RecordActivity component with modal for activity recording, and update Login component for improved ID validation. Making changes to the activity model to align with the database

--- ActimaticWebApp/Program.cs
+++ ActimaticWebApp/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddRazorComponents()
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5205/") });
 builder.Services.AddScoped<AppInterfaces.IAuthService, JwtAuthService>();
 builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ActivitiesService>();
 builder.Services.AddDbContext<ApplicationDbContext>();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthProvider>();
