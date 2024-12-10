ece81b9712584b0c6265be1207251e20dc985b7e
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2024-12-10 17:12:18 +0100

    Fixing merge

--- ActimaticAPI/Services/TransportService.cs
+++ ActimaticAPI/Services/TransportService.cs
@@ -8,16 +8,13 @@ using Microsoft.EntityFrameworkCore;
 
 
 namespace Services;
-public class TransportService(ApplicationDbContext context) : ITransportService
-{
-    private readonly ApplicationDbContext _context = context;
-public class TransportService(ApplicationDbContext context) : ITransportService
-{
-    private readonly ApplicationDbContext _context = context;
-
-    static TransportService()
+ public class TransportService(ApplicationDbContext context)  : ITransportService
     {
-    }
+        private readonly ApplicationDbContext _context = context;
+
+        static TransportService()
+        {
+        }
     private const int BasePointsPerKm = 1;
 
     private int CalculatePoints(Transport transport)
@@ -34,7 +31,7 @@ public class TransportService(ApplicationDbContext context) : ITransportService
         return basepoints + bonusPoints;
     }
 
-    public async Task<Transport> CreateTransport(Transport transport)
+     public async Task<Transport> CreateTransport(Transport transport)
     {
         transport.AwardedPoints = CalculatePoints(transport);
         _context.Transports.Add(transport);
@@ -42,83 +39,39 @@ public class TransportService(ApplicationDbContext context) : ITransportService
         return transport;
     }
     public async Task<IEnumerable<Transport>> GetAllTransport()
-    {
-        return await _context.Transports.ToListAsync();
-    }
-    {
-        return await _context.Transports.ToListAsync();
-    }
+        {
+            return await _context.Transports.ToListAsync();
+        }
 
-<<<<<<< HEAD
-    public async Task<Transport> GetTransportById(int id)
-    {
-        return await _context.Transports.FindAsync(id);
-    }
-    public async Task<Transport> GetTransportById(int id)
-    {
-        return await _context.Transports.FindAsync(id);
-    }
-=======
-   public async Task<Transport?> GetTransportById(int id)
+   public async Task<Transport> GetTransportById(int id)
         {
             return await _context.Transports.FindAsync(id);
         }
->>>>>>> Ada
 
     public async Task<Transport> RemoveTransport(int id)
-    {
-        var transport = await _context.Transports.FindAsync(id);
-        _context.Transports.Remove(transport);
-        await _context.SaveChangesAsync();
-        return await Task.FromResult(transport);
-    }
+        {
+            var transport = await _context.Transports.FindAsync(id);
+           _context.Transports.Remove(transport);
+                await _context.SaveChangesAsync();
+                return await Task.FromResult(transport);
+            }
+        
 
-    {
-        var transport = await _context.Transports.FindAsync(id);
-        _context.Transports.Remove(transport);
-        await _context.SaveChangesAsync();
-        return await Task.FromResult(transport);
-    }
-
-
-<<<<<<< HEAD
     public async Task<Transport> UpdateTransport(Transport transport)
-    {
-        var transportToUpdate = await _context.Transports.FindAsync(transport.Id);
-        if (transportToUpdate != null)
-=======
-    public async Task<Transport?> UpdateTransport(Transport transport)
->>>>>>> Ada
         {
-            transportToUpdate.Name = transport.Name;
-            transportToUpdate.AwardedPoints = transport.AwardedPoints;
-            transportToUpdate.Date = transport.Date;
-            transportToUpdate.Distance = transport.Distance;
-            transportToUpdate.Type = transport.Type;
-            transportToUpdate.EmissionsSaved = transport.EmissionsSaved;
+            var transportToUpdate = await _context.Transports.FindAsync(transport.Id);
+            if (transportToUpdate != null)
+            {
+                transportToUpdate.Name = transport.Name;
+                transportToUpdate.AwardedPoints = transport.AwardedPoints;
+                transportToUpdate.Date = transport.Date;
+                transportToUpdate.Distance = transport.Distance;
+                transportToUpdate.Type = transport.Type;
+                transportToUpdate.EmissionsSaved = transport.EmissionsSaved;
+            }
+                await _context.SaveChangesAsync();
+            
+            return await Task.FromResult(transportToUpdate);
         }
-        await _context.SaveChangesAsync();
-
-        return await Task.FromResult(transportToUpdate);
+        
     }
-}
-    {
-        var transportToUpdate = await _context.Transports.FindAsync(transport.Id);
-        if (transportToUpdate != null)
-        {
-            transportToUpdate.Name = transport.Name;
-            transportToUpdate.AwardedPoints = transport.AwardedPoints;
-            transportToUpdate.Date = transport.Date;
-            transportToUpdate.Distance = transport.Distance;
-            transportToUpdate.Type = transport.Type;
-            transportToUpdate.EmissionsSaved = transport.EmissionsSaved;
-        }
-        await _context.SaveChangesAsync();
-
-        return await Task.FromResult(transportToUpdate);
-    }
-    public async Task<IEnumerable<Transport>> GetTransportByDatesAsync(DateOnly startDate, DateOnly endDate)
-    {
-        return await Task.FromResult(_context.Transports.Where(t => t.Date >= startDate && t.Date <= endDate).AsEnumerable());
-    }
-}
