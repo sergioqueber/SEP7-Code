235ffcc54d9ceeeaf2bc302ccb92afdd2b41425f
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2024-12-10 11:32:59 +0100

    Add parameterless constructors to activity models: CarPool, SavingFood, Stairs, Transport, and Volunteering

--- ActimaticAPI/Model/SavingFood.cs
+++ ActimaticAPI/Model/SavingFood.cs
@@ -3,6 +3,7 @@ public class SavingFood : Activity
 {
    public int PackagesSaved { get; set; }
    
+   public SavingFood() { }
     public SavingFood(int id, string name, int awardedPoints, DateOnly date, int packagesSaved) : base(id, name, awardedPoints, date)
     {
        PackagesSaved = packagesSaved;
